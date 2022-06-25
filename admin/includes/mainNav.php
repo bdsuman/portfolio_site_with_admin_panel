da09098d77c605213f3dd6667bc0c8edc366963d
Suman Sen <mesuman@yahoo.com>
2022-06-26 00:34:13 +0600

    picture add path ok now

--- admin/includes/mainNav.php
+++ admin/includes/mainNav.php
@@ -1,7 +1,7 @@
 <!-- Main navbar -->
 <div class="navbar navbar-inverse">
     <div class="navbar-header">
-        <a class="navbar-brand" href="#"><img src="assets/images/logo_light.png" alt=""></a>
+        <a class="navbar-brand" href="#"><img src="<?php echo $isInternal == true ? '../': ' ';?>assets/images/logo_light.png" alt="hiiiiiiiiiiii"></a>
 
         <ul class="nav navbar-nav visible-xs-block">
             <li><a data-toggle="collapse" data-target="#navbar-mobile"><i class="icon-tree5"></i></a></li>
@@ -18,7 +18,7 @@
         <ul class="nav navbar-nav navbar-right">
             <li class="dropdown dropdown-user">
                 <a class="dropdown-toggle" data-toggle="dropdown">
-                    <img src="assets/images/placeholder.jpg" alt="">
+                    <img src="<?php echo $isInternal == true ? '../': '';?>assets/images/placeholder.jpg" alt="">
                     <span>Victoria</span>
                     <i class="caret"></i>
                 </a>
